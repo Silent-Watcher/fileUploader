23876506dfa2cfbd7ba02ad4f6c6c719fd817e89
Ali t.nazari <alitabatabaee20@gmail.com>
2023-10-31 20:00:43 +0330

    feat:add header and main section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./views/**/*.ejs'],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+		},
+		extend: {
+			colors:{
+				primary: '#68a063',
+			}
+		},
 	},
 	plugins: [],
 };
