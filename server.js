e63977f185573baafbe27aa431244fc5f5681e67
Ali t.nazari <alitabatabaee20@gmail.com>
2023-10-31 19:29:27 +0330

    add tailwind css as the main css frameword

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
+const { join } = require('path');
+
 const app = express();
 require('./config/env.config');
 
 const port = process.env.PORT;
 
 app.set('view engine', 'ejs');
-app.use('/public', express.static('./public'));
+app.set('views', join(__dirname, 'views'));
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
-	res.locals = {title: 'file uploader'};
+	res.locals = { title: 'file uploader' };
 	next();
 });
 
 app.get('/', (req, res) => {
 	res.status(200).render('index');
 });
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
